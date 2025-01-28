ec842be493a59acc8f5319a566378a6a70108c96
CommanderCortex <CommanderCortex@proton.me>
2025-01-28 13:56:08 +1300

    Updated Serial Driver to be more safe ?

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,20 @@ mod serialdriver;
 static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
 const STACK_SIZE: usize = 1024;
+const SERIAL_ADDR: *mut u8 = 0x0900_0000 as *mut u8;
 
 global_asm!(include_str!("boot.s"), sym STACK, const STACK_SIZE);
 
 //
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {	
-	serial_put_string("Kernel Panic: Halt!\n");
 	loop{}
 }
 
 #[no_mangle]
 fn main() {
-	serial_put_string("Hello, world!\n");
+	let mut s = SerialDriver::new(SERIAL_ADDR);
+	s.serial_put_string("Hello, World!\n");
 	panic!();
-	
-	loop{}
+	//loop{}
 }	
--- src/serialdriver.rs
+++ src/serialdriver.rs
@@ -1,12 +1,21 @@
-pub fn serial_put_string(s: &str) {
-	for c in s.chars() {
-		serial_put_char(c);
-	}
+pub struct SerialDriver{
+	uart_addr: *mut u8
 }
-
-pub fn serial_put_char(c: char) {
-	let uart_addr = 0x0900_0000 as *mut u8; 
-	unsafe {
-		uart_addr.write_volatile(c as u8);
+impl SerialDriver{
+	pub fn new(uart_addr: *mut u8) -> Self{
+		SerialDriver{
+			uart_addr: uart_addr,
+		}
+	}
+	pub fn serial_put_string(&mut self, s: &str) {
+		for c in s.chars() {
+			self.serial_put_char(c);
+		}
+	}
+	
+	pub fn serial_put_char(&mut self, c: char) {
+		unsafe {
+			self.uart_addr.write_volatile(c as u8);
+		}
 	}
 }
